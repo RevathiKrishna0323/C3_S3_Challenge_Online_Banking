8950836fda67428b8beee8930e11f25c3cf3975b
Revathi G <revathikrishna0323@gmail.com>
2022-08-25 23:24:13 +0530

    declared some attributes that can be modelled in SavingsAccount.java class

--- src/main/java/com/niit/jdp/SavingsAccount.java
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -7,4 +7,17 @@
 package com.niit.jdp;
 
 public class SavingsAccount {
+    //some attributes that can be modelled in SavingsAccount class are
+    private String accountName;
+    private String accountNumber;
+    private String accountOpeningDate;
+    private String status;
+    private String availableBalance;
+    private String totalBalance;
+    private Boolean isInternetBankingEnabled;
+    private Boolean isMobileBankingEnabled;
+    private double interestRate;
+    private String modeOfOperation;
+
+
 }
