cfc3b797b9f6e2889d491d506612216af37375af
Revathi G <revathikrishna0323@gmail.com>
2022-08-25 23:25:35 +0530

    Created getter and setter methods for all the attributes which are modelled in SavingsAccount.java class

--- src/main/java/com/niit/jdp/SavingsAccount.java
+++ src/main/java/com/niit/jdp/SavingsAccount.java
@@ -18,6 +18,84 @@ public class SavingsAccount {
     private Boolean isMobileBankingEnabled;
     private double interestRate;
     private String modeOfOperation;
+    public String getAccountName() {
+        return accountName;
+    }
 
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountOpeningDate() {
+        return accountOpeningDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getAvailableBalance() {
+        return availableBalance;
+    }
+
+    public String getTotalBalance() {
+        return totalBalance;
+    }
+
+    public Boolean getInternetBankingEnabled() {
+        return isInternetBankingEnabled;
+    }
+
+    public Boolean getMobileBankingEnabled() {
+        return isMobileBankingEnabled;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public String getModeOfOperation() {
+        return modeOfOperation;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void setAccountOpeningDate(String accountOpeningDate) {
+        this.accountOpeningDate = accountOpeningDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setAvailableBalance(String availableBalance) {
+        this.availableBalance = availableBalance;
+    }
+
+    public void setTotalBalance(String totalBalance) {
+        this.totalBalance = totalBalance;
+    }
+
+    public void setInternetBankingEnabled(Boolean internetBankingEnabled) {
+        isInternetBankingEnabled = internetBankingEnabled;
+    }
+
+    public void setMobileBankingEnabled(Boolean mobileBankingEnabled) {
+        isMobileBankingEnabled = mobileBankingEnabled;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void setModeOfOperation(String modeOfOperation) {
+        this.modeOfOperation = modeOfOperation;
+    }
 
 }
